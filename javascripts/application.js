c49d3d5c6bfed4fd5fb9048c8df6c0cfa0f77cfb
Sam Willis <sam.willis@event.ly>
2013-12-01 14:40:44 +0000

    style sidebar

--- javascripts/application.js
+++ javascripts/application.js
@@ -1,3 +1,13 @@
+// HANGOUTS JS
+
+
+// PAGE LOAD STATE
+
+$('.close-nav').hide();
+
+
+// SUPERSLIDES
+
 $(document).ready(function() {
   $('#slides').superslides({
     slide_easing: 'easeInOutCubic',
@@ -7,3 +17,24 @@ $(document).ready(function() {
     scrollable: true
   });
 });
+
+
+// SNAPPER SIDEBAR
+
+var snapper = new Snap({
+  element: document.getElementById('content'),
+  disable: 'left',
+  tapToClose: false
+});
+
+$('body').on('click', '.open-nav', function() {
+  snapper.open('right');
+  $('.open-nav').hide();
+  $('.close-nav').show();
+})
+
+$('body').on('click', '.close-nav', function() {
+  snapper.close('right');
+  $('.close-nav').hide();
+  $('.open-nav').show();
+})
