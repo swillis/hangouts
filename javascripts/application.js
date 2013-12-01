87527acdac4e19b17bd97eb2b0a2879b2a260ef0
Sam Willis <sam.willis@event.ly>
2013-12-01 16:14:53 +0000

    Styling fancy title, slideshow nav and flyout

--- javascripts/application.js
+++ javascripts/application.js
@@ -5,6 +5,44 @@
 
 $('.close-nav').hide();
 
+$('nav').hide();
+
+$('.fancy-title').hide();
+
+
+// INTRO
+
+$(window).load(function() {
+  $('.fancy-title').fadeIn(500, function() {
+    $('.fancy-title').delay(1200).fadeOut(500, function() {
+      $('nav').fadeIn(500);
+    });
+  });
+})
+
+
+// CENTER FANCY TITLE
+
+$(function() {
+  var headHeight = $('.fancy-title').height()/2;
+  var windowHeight = $(window).height();
+  var halfWay = windowHeight/2;
+  var navHeight = $('nav').height();
+  $('.fancy-title').css({
+    'padding-top': halfWay - headHeight - navHeight
+  })
+})
+
+$(window).resize(function() {
+  var headHeight = $('.fancy-title').height()/2;
+  var windowHeight = $(window).height();
+  var halfWay = windowHeight/2;
+  var navHeight = $('nav').height();
+  $('.fancy-title').css({
+    'padding-top': halfWay - headHeight - navHeight
+  })
+})
+
 
 // SUPERSLIDES
 
@@ -37,4 +75,12 @@ $('body').on('click', '.close-nav', function() {
   snapper.close('right');
   $('.close-nav').hide();
   $('.open-nav').show();
+})
+
+
+// FLYOUTS
+
+$('body').on('click', '.sidebar li a', function() {
+  $('.flyout').animate({'bottom' : '-600'}, 200);
+  $('.venue-name').addClass('active');
 })
